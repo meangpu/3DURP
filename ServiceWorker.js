dd0d932355228ddaf0f71cb5c3b92c19426e463e
meangpu <meangpu@gmail.com>
2024-10-08 00:34:30 +0700

    update PWA

---
+++ ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "meangpu-U.Base3DURP-0.65-8-Oct-2024 12:32 AM";
+const contentToCache = [
+    "Build/3DURP.loader.js",
+    "Build/3DURP.framework.js.unityweb",
+    "Build/3DURP.data.unityweb",
+    "Build/3DURP.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
