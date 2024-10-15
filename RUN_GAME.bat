9886c098fd40799c98511f678a8bebb8a77978f0
meangpu <meangpu@gmail.com>
2024-10-15 19:24:18 +0700

    tb

--- RUN_GAME.bat
+++ RUN_GAME.bat
@@ -1,15 +1,6 @@
 @echo off
-REM Change to the directory containing this batch file
 cd /d %~dp0
-
-REM Start Python HTTP server without showing a window
-start /B "" pythonw -m http.server 8000 >nul 2>&1
-
-REM Wait for a moment to ensure the server has started
-timeout /t 2 > nul
-
-REM Open the default browser to localhost:8000
+start /B "" pythonw -m http.server 8000 >nul 0>&1
+timeout /t 0 > nul
 start http://localhost:8000
-
-REM Exit the batch file
 exit
